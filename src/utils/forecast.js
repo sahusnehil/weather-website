97ddc8e803963785b36ba595024015fc2d7658ab
sahusnehil <sahusnehil007@gmail.com>
2021-03-29 04:30:51 +0530

    Added info of Humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " Celsius approx out.")
+            
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degree approx out.It feels like "+body.current.feelslike +" degree out. The humidity is "+ body.current.humidity +"%.")
         }
     })
 }
